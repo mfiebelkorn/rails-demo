cd097db8f9cbc54e2145a7dee54b949ccac67ac6
Michael <mfiebelkorn@hca-michaelsbisch.(none)>
2017-08-18 09:20:46 -0500

    Updated database to use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170817204800) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "counters", force: :cascade do |t|
     t.string "name"
     t.integer "value"
